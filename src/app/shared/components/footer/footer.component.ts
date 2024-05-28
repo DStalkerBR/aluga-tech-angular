131d41a1930b2236fafc3c2dbae24402b66e92fc
DStalkerBR <jhon89araujo@gmail.com>
2024-05-28 19:11:18 -0300

    Add initial footer, home and header components

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {}

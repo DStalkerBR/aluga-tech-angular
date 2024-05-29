26311994a9b9801a5c2fb022272e704a59db02d8
DStalkerBR <jhon89araujo@gmail.com>
2024-05-28 23:27:19 -0300

    Add user manual page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UserManualComponent } from './pages/user-manual/user-manual.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'user-manual', component: UserManualComponent },
   { path: '**', redirectTo: '' },
 ];

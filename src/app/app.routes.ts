2c845f734161d5cb12616ddd1614c64d93970a84
DStalkerBR <jhon89araujo@gmail.com>
2024-05-29 22:19:35 -0300

    add about route and auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { DeviceEditComponent } from './shared/components/device-edit/device-edit
 import { NgModule } from '@angular/core';
 import { DeviceListComponent } from './shared/components/device-list/device-list.component';
 import { UserManualComponent } from './pages/user-manual/user-manual.component';
+import { AuthGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'devices', component: DeviceListComponent },
-  { path: 'devices/:id/edit', component: DeviceEditComponent },
-  { path: 'devices/:id', component: DeviceDetailComponent },
+  { path: 'devices', component: DeviceListComponent, canActivate: [AuthGuard] },
+  { path: 'devices/:id/edit', component: DeviceEditComponent, canActivate: [AuthGuard] },
+  { path: 'devices/:id', component: DeviceDetailComponent, canActivate: [AuthGuard] },
   { path: 'user-manual', component: UserManualComponent },
   { path: '**', redirectTo: '' },
 ];
--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -17,7 +21,10 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
-      this.router.navigate(['/login']);
+      this.snackBar.open('Você precisa estar logado para acessar essa página', 'OK', {
+        duration: 5000,
+      });
+      this.router.navigate(['/home']);
       return false;
     }
   }

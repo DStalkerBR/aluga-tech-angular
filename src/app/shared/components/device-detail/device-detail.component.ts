440d67814d3c7df691d852bb5746095d25543bce
DStalkerBR <jhon89araujo@gmail.com>
2024-05-30 01:18:18 -0300

    add visual rent functionality

--- src/app/shared/components/device-detail/device-detail.component.ts
+++ src/app/shared/components/device-detail/device-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import { Device } from '../../../models/interfaces/device.model';
 import { DeviceService } from '../../../core/services/device.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -6,14 +6,21 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-device-detail',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     MatCardModule,
+    MatFormFieldModule,
     MatButtonModule,
+    MatSelectModule,
     MatProgressSpinnerModule,
   ],
   templateUrl: './device-detail.component.html',
@@ -22,12 +29,24 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 export class DeviceDetailComponent implements OnInit {
   device!: Device;
   isLoading = true;
+  selectedDays: number;
+  selectedDaySignal: any;
+  daysOptions: number[];
+  subTotalSignal: any;
 
   constructor(
     private route: ActivatedRoute,
     private deviceService: DeviceService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
+    this.selectedDays = 1;
+    this.daysOptions = Array.from({ length: 30 }, (_, i) => i + 1);
+    this.selectedDaySignal = signal(this.selectedDays);
+    this.subTotalSignal = computed(
+      () => this.device.rentalPrice * this.selectedDaySignal()
+    );
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -37,7 +56,37 @@ export class DeviceDetailComponent implements OnInit {
     });
   }
 
+  updateDays() {
+    this.selectedDaySignal.set(this.selectedDays);
+  }
+
   goBack(): void {
     this.router.navigate(['/devices']);
+  }
+
+  rentDevice() {
+    this.device.status = 'rented';
+    this.device.availableFrom = new Date();
+    this.device.availableFrom.setDate(
+      this.device.availableFrom.getDate() + this.selectedDays
+    );
+    this.device.availableTo = this.device.availableFrom;
+    this.deviceService.updateDevice(this.device).subscribe({
+      next: () => {
+        this.snackBar.open('Dispositivo alugado com sucesso!', 'Fechar', {
+          duration: 3000,
+        });
+        this.router.navigate(['/devices']);
+      },
+      error: (error) => {
+        this.snackBar.open(
+          'Erro ao alugar dispositivo. Tente novamente.',
+          'Fechar',
+          {
+            duration: 3000,
+          }
+        );
+      },
+    });
   }
 }
